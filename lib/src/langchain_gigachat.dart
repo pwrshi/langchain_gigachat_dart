9dbceac6183833f1e66fb9ad5f70878d9ca7bb28
pwrshi <me@pwrshi.ru>
2024-05-02 18:56:47 +0300

    init

---
+++ lib/src/langchain_gigachat.dart
@@ -0,0 +1,7 @@
+/// {@template langchain_gigachat}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class LangchainGigachat {
+  /// {@macro langchain_gigachat}
+  const LangchainGigachat();
+}
---
+++ test/src/langchain_gigachat_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:langchain_gigachat/langchain_gigachat.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('LangchainGigachat', () {
+    test('can be instantiated', () {
+      expect(LangchainGigachat(), isNotNull);
+    });
+  });
+}
