9a9fe7a0a26fcc8ed0a2dce5cfb8caf25b1daa60
pwrshi <me@pwrshi.ru>
2024-05-22 20:20:19 +0300

    docs fixed

--- lib/langchain_gigachat.dart
+++ lib/langchain_gigachat.dart
@@ -1,3 +1,21 @@
+/// # 🦜️🔗 LangChain.dart / GigaChat (Unofficial)
+///
+/// Unofficial [GigaChat](https://developers.sber.ru/docs/ru/gigachat/overview)
+///  module for [LangChain.dart](https://github.com/davidmigloz/langchain_dart).
+
+/// ## Features
+
+/// - LLMs:
+///   * `GigaChat`: wrapper around GigaChat Completions API.
+/// - Chat models:
+///   * `ChatGigaChat`: wrapper around GigaChat Completions API in a chat-like
+///  fashion.
+/// - Embeddings:
+///   * `GigaChatEmbeddings`: wrapper around GigaChat Embeddings API.
+///  (not tested)
+
+library;
+
 export './src/chat_models/chat_models.dart';
 export './src/embeddings/embeddings.dart';
 export './src/llms/llms.dart';
